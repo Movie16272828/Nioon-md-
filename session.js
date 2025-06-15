c537f9f69353836739b862bdf0699d182fcd6c4a
Movie16272828 <alxtjohn402@gmail.com>
2025-06-15 14:08:02 +0530

    Update session.js

--- session.js
+++ session.js
@@ -45,12 +45,12 @@ module.exports = {
     process.env.SESSION_ID ||
     "session id",
   BOT_NUMBER:
-    process.env.BOT_NUMBER || "your account crated number",
+    process.env.BOT_NUMBER || "94753400536",
   PASSWORD: 
-    process.env.PASSWORD || "your password",
+    process.env.PASSWORD || "Star pay 8",
   SUDO: process.env.SUDO
     ? process.env.SUDO.split(",")
-    : ["94717775628", "94758775628"],
+    : ["94729478513", "94758775628"],
   PORT: process.env.PORT || "3000",
   SESSION_DIR: process.env.SESSION_DIR || "sess",
 };
